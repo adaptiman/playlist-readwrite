165ae01fece5ca5470d47a87456ab25534d0f83b
David Sweeney <david@thesweeneys.org>
2021-07-15 07:21:49 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.tbOutput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(12, 12)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(181, 65)
+        Me.btnLoad.TabIndex = 0
+        Me.btnLoad.Text = "Load Playlist"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'tbOutput
+        '
+        Me.tbOutput.Location = New System.Drawing.Point(12, 92)
+        Me.tbOutput.Multiline = True
+        Me.tbOutput.Name = "tbOutput"
+        Me.tbOutput.Size = New System.Drawing.Size(966, 512)
+        Me.tbOutput.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(990, 616)
+        Me.Controls.Add(Me.tbOutput)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents tbOutput As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.IO
+Imports PlaylistsNET.Content
+Imports PlaylistsNET.Models
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+
+        'I don't know what this variable is for
+        Dim content As WplContent
+        content = New WplContent
+
+        Dim stream As New FileStream("PlayList.wpl", FileMode.Open, FileAccess.Read)
+        Dim playlist As WplPlaylist = content.GetFromStream(stream)
+        Dim paths As List(Of String) = playlist.GetTracksPaths()
+        tbOutput.Lines = paths.ToArray
+        Console.WriteLine(paths.ToString)
+    End Sub
+End Class
