13c6e523b4e1ec0138812d591f6e7e231a032cef
David Sweeney <david@thesweeneys.org>
2021-07-18 09:07:04 -0500

    Adds streamwriter code.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,41 +22,103 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnLoad = New System.Windows.Forms.Button()
         Me.tbOutput = New System.Windows.Forms.TextBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.tbFilePath = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.btnSelectFile = New System.Windows.Forms.Button()
+        Me.tbTitle = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnAddCue = New System.Windows.Forms.Button()
         Me.SuspendLayout()
-        '
-        'btnLoad
-        '
-        Me.btnLoad.Location = New System.Drawing.Point(12, 12)
-        Me.btnLoad.Name = "btnLoad"
-        Me.btnLoad.Size = New System.Drawing.Size(181, 65)
-        Me.btnLoad.TabIndex = 0
-        Me.btnLoad.Text = "Load Playlist"
-        Me.btnLoad.UseVisualStyleBackColor = True
         '
         'tbOutput
         '
         Me.tbOutput.Location = New System.Drawing.Point(12, 92)
         Me.tbOutput.Multiline = True
         Me.tbOutput.Name = "tbOutput"
-        Me.tbOutput.Size = New System.Drawing.Size(966, 512)
+        Me.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbOutput.Size = New System.Drawing.Size(966, 779)
         Me.tbOutput.TabIndex = 1
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(797, 12)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(181, 65)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save File"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'tbFilePath
+        '
+        Me.tbFilePath.Location = New System.Drawing.Point(208, 911)
+        Me.tbFilePath.Name = "tbFilePath"
+        Me.tbFilePath.Size = New System.Drawing.Size(770, 31)
+        Me.tbFilePath.TabIndex = 3
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'btnSelectFile
+        '
+        Me.btnSelectFile.Location = New System.Drawing.Point(12, 894)
+        Me.btnSelectFile.Name = "btnSelectFile"
+        Me.btnSelectFile.Size = New System.Drawing.Size(181, 65)
+        Me.btnSelectFile.TabIndex = 5
+        Me.btnSelectFile.Text = "Select File"
+        Me.btnSelectFile.UseVisualStyleBackColor = True
+        '
+        'tbTitle
+        '
+        Me.tbTitle.Location = New System.Drawing.Point(208, 972)
+        Me.tbTitle.Name = "tbTitle"
+        Me.tbTitle.Size = New System.Drawing.Size(770, 31)
+        Me.tbTitle.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(140, 975)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 25)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Title"
+        '
+        'btnAddCue
+        '
+        Me.btnAddCue.Location = New System.Drawing.Point(12, 12)
+        Me.btnAddCue.Name = "btnAddCue"
+        Me.btnAddCue.Size = New System.Drawing.Size(181, 65)
+        Me.btnAddCue.TabIndex = 8
+        Me.btnAddCue.Text = "Add Cue"
+        Me.btnAddCue.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(990, 616)
+        Me.ClientSize = New System.Drawing.Size(1001, 1034)
+        Me.Controls.Add(Me.btnAddCue)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tbTitle)
+        Me.Controls.Add(Me.btnSelectFile)
+        Me.Controls.Add(Me.tbFilePath)
+        Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.tbOutput)
-        Me.Controls.Add(Me.btnLoad)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "WPL Read/Write"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents btnLoad As Button
     Friend WithEvents tbOutput As TextBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents tbFilePath As TextBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents btnSelectFile As Button
+    Friend WithEvents tbTitle As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnAddCue As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -3,20 +3,94 @@ Imports PlaylistsNET.Content
 Imports PlaylistsNET.Models
 
 Public Class Form1
+
+    'content is an object that holds all of the playlist data
+    Dim content As New WplContent
+
+    Dim playlist As New WplPlaylist
+
+    'paths stores all of the file paths to play
+    Dim paths As New List(Of String)
+
+    Private Sub Load_Data()
+
+        'writes the content to the textbox
+        tbOutput.Text = content.ToText(playlist)
+
+        'writes the content to Output
+        Console.WriteLine(content.ToText(playlist))
+
+    End Sub
+
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
-
-        'I don't know what this variable is for
-        Dim content As WplContent
-        content = New WplContent
-
+        'stream is used to access the file
         Dim stream As New FileStream("PlayList.wpl", FileMode.Open, FileAccess.Read)
-        Dim playlist As WplPlaylist = content.GetFromStream(stream)
-        Dim paths As List(Of String) = playlist.GetTracksPaths()
-        tbOutput.Lines = paths.ToArray
-        Console.WriteLine(paths.ToString)
+        'Dim myStreamWriter As New StreamWriter(stream)
+
+        'loads the wpl content into the playlist
+        playlist = content.GetFromStream(stream)
+
+        'loads the paths from playlist
+        'paths = playlist.GetTracksPaths()
+
+        Load_Data()
+
+        stream.Close()
+
     End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+        'stream is used to access the file
+        Dim stream As New FileStream("PlayList.wpl", FileMode.Truncate, FileAccess.Write)
+        'Dim myStreamWriter As New StreamWriter(stream, Encoding.Default, 4096)
+        Dim myStreamWriter As New StreamWriter(stream)
+
+        'Writes content to the file
+        myStreamWriter.WriteLine(content.ToText(playlist))
+        myStreamWriter.Flush()
+
+        Load_Data()
+
+        'close the stream
+        stream.Close()
+
+    End Sub
+
+    Private Sub btnSelectFile_Click(sender As Object, e As EventArgs) Handles btnSelectFile.Click
+
+        Dim result As DialogResult = OpenFileDialog1.ShowDialog()
+
+        ' Test result.
+        If result = Windows.Forms.DialogResult.OK Then
+            tbFilePath.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
+
+    Private Sub btnAddCue_Click(sender As Object, e As EventArgs) Handles btnAddCue.Click
+
+        Dim entry As WplPlaylistEntry
+        entry = New WplPlaylistEntry With {
+            .AlbumArtist = "Unknown Artist",
+            .TrackArtist = "Unknown Artist",
+            .AlbumTitle = "Unknown Album",
+            .Path = tbFilePath.Text,
+            .TrackTitle = tbTitle.Text
+        }
+
+        playlist.PlaylistEntries.Add(entry)
+
+        tbFilePath.Text = ""
+        tbOutput.Text = ""
+
+        Load_Data()
+
+    End Sub
+
 End Class
